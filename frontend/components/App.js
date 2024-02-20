357cb99677444aad38ca032a8c124340494a3616
Alex Handley <smithvillescholarsah@gmail.com>
2024-02-19 20:14:56 -0600

    S9M1-Project completed

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,11 +1,49 @@
 import React from 'react'
+import Form from "./Form";
+import TodoList from './TodoList';
+
+let id = 0;
+function makeId() {
+return ++id;
+}
+
+const initialTodos = [
+  { id: makeId(), name: "Walk the dog", completed: false },
+  { id: makeId(), name: "Learn React", completed: true },
+  { id: makeId(), name: "Have fun", completed: false }
+];
 
 export default class App extends React.Component {
+  state = {
+    todos: initialTodos
+  };
+
+  // making the ability to add new tasks to the todos object
+  addTodo = (name) => {
+    this.setState({
+      ...this.state,
+      todos: this.state.todos.concat({ id: makeId(), completed: false, name })
+    });
+  };
+
+  // controlling if a task is completed or not
+  toggleCompletion = (id) => {
+    this.setState({
+      ...this.state,
+      todos: this.state.todos.map((items) => {
+        if (id === items.id) return { ...items, completed: !items.completed };
+        return items;
+      })
+    });
+  };
+
   render() {
     return (
       <div>
-        Todo App
+        <h2>Todos:</h2>
+        <TodoList todos={this.state.todos} toggleCompletion={this.toggleCompletion} />
+        <Form addTodo={this.addTodo} />
       </div>
     )
-  }
+  };
 }
--- frontend/components/TodoList.js
+++ frontend/components/TodoList.js
@@ -1,10 +1,29 @@
 import React from 'react'
+import Todo from "./Todo";
 
 export default class TodoList extends React.Component {
+  state = {
+    showAll: true
+  };
+
+  toggleShowAll = (event) => {
+    this.setState({
+      ...this.state,
+      showAll: !this.state.showAll
+    })
+  }
+
   render() {
+    const { todos, toggleCompletion } = this.props;
+    const filtered = todos.filter((item) => !item.completed || this.state.showAll)
     return (
       <div>
-        TodoList
+        {
+          filtered.map((item) => (
+            <Todo key={item.id} todo={item} toggleCompletion={toggleCompletion} />
+          ))
+        }
+        <button onClick={this.toggleShowAll}>{this.state.showAll ? "Hide" : "Show"} Completed</button>
       </div>
     )
   }
